df0fc6261161291339e7615cfca7c5ab59de8b11
Amirul <amirul.ardy@gmail.com>
2015-05-16 19:34:18 +0800

    enabled update() to insert multiple entries

--- exercises/pc06-game-linked-list/task_1.py
+++ exercises/pc06-game-linked-list/task_1.py
@@ -23,28 +23,43 @@ def val_id(player_id):
 
 
 def update(linkedlist):
-    player_id = input("Enter player ID: ")
-    score = input("Enter score: ")
+    while True:
+        print(linkedlist)  # display the current list
 
-    if val_id(player_id) and val_score(score):
+        while True:  # get player ID
+            player_id = input("Enter player ID (0 to exit): ")
+            if player_id == '0':
+                return  # EXIT
+            elif not val_id(player_id):
+                print("\n- Invalid player ID -\n")
+            else:
+                break  # all okay
 
-        print("\nOld rank: ", linkedlist.get_rank(player_id))
+        while True:  # get score
+            score = input("Enter score for {0}: ".format(player_id))
+            if not val_score(score):
+                print("\n- Invalid score -\n")
+            else:
+                break  # all okay
 
+        # header
+        print("\nPlayer: ", player_id)
+        print('-'*20)
+
+        print("Old rank: ", linkedlist.get_rank(player_id))
+
+        # updating list with new player/score
         if linkedlist.exists(player_id):
             new_score = linkedlist.get_score(player_id) + int(score)
-            # remove player node and readd to place player node into the new
-            # correct position
+            # remove player node and readd to place player node into the
+            # new correct position
             linkedlist.remove(player_id)
             linkedlist.add(player_id, new_score)
         else:
             linkedlist.add(player_id, score)
 
         print("New rank: ", linkedlist.get_rank(player_id))
-
-    else:
-        print("\n- Invalid input -")
+        print()
 
 
-# print(SCORE_LIST)  # BEFORE
 # update(SCORE_LIST)
-# print(SCORE_LIST)  # AFTER
