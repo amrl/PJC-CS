494f44779aee64a4f7c75a8b1c99510e919250b0
Amirul <amirul.ardy@gmail.com>
2015-05-17 20:26:31 +0800

    enabled multiple input tries for display_rank()

--- exercises/pc06-game-linked-list/task_3.py
+++ exercises/pc06-game-linked-list/task_3.py
@@ -24,47 +24,48 @@ def val_rank_range(rank_range, linkedlist):
 
 
 def display_rank(linkedlist):
-    rank_range = input("Enter a rank range (e.g. 1-3): ")
+    while True:
+        rank_range = input("Enter a rank range (e.g. 1-3): ")
+        if not val_rank_range(rank_range, linkedlist):
+            print("\n- Invalid input -\n")
+        else:
+            break
 
     ranked_list = []  # list of tuples [(rank1, player1), (rank2, player2),...]
     rank_count = {}   # {rank1:count1, rank2:count2,...}
 
-    if val_rank_range(rank_range, linkedlist):
-        lower, upper = [int(i) for i in rank_range.split('-')]
+    lower, upper = [int(i) for i in rank_range.split('-')]
 
-        current = linkedlist.head
-        while current is not None:
-            current_rank = linkedlist.get_rank(current.id)
+    current = linkedlist.head
+    while current is not None:
+        current_rank = linkedlist.get_rank(current.id)
 
-            if current_rank >= lower and current_rank <= upper:
-                ranked_list.append((current_rank, current.id))
-                rank_count[current_rank] = rank_count.get(current_rank, 0) + 1
-            elif current_rank > upper:
-                break
+        if current_rank >= lower and current_rank <= upper:
+            ranked_list.append((current_rank, current.id))
+            rank_count[current_rank] = rank_count.get(current_rank, 0) + 1
+        elif current_rank > upper:
+            break
 
-            current = current.next
+        current = current.next
 
-        # display rank & player ID header
-        print("\n{0:^10}{1}{2:^15}".format("Rank", '|', "Player ID"))
-        print('-'*25)
+    # display rank & player ID header
+    print("\n{0:^10}{1}{2:^15}".format("Rank", '|', "Player ID"))
+    print('-'*25)
 
-        # display real data
-        for data in ranked_list:
-            print("{0:^10}{1}{2:^15}".format(data[0], '|', data[1]))
+    # display rank & player ID
+    for data in ranked_list:
+        print("{0:^10}{1}{2:^15}".format(data[0], '|', data[1]))
 
-        print()
+    print()
 
-        # display rank count header
-        print("{0:^25}".format("Count"))
-        print('-'*25)
+    # display rank count header
+    print("{0:^25}".format("Count"))
+    print('-'*25)
 
-        # display real data
-        for rank in range(lower, upper+1):
-            if rank in rank_count:
-                print("Rank #{0}: {1}".format(rank, rank_count[rank]))
-
-    else:
-        print("- Invalid input -")
+    # display rank count
+    for rank in range(lower, upper+1):
+        if rank in rank_count:
+            print("Rank #{0}: {1}".format(rank, rank_count[rank]))
 
 
 # display_rank(SCORE_LIST)
