1d814a53efd0bd9e96f9d3569d79b662cc0174f6
Amirul <amirul.ardy@gmail.com>
2015-09-27 21:35:35 +0800

    Add methods from Task 4.2

--- exercises/2015-dunman-prelim/task-4/everything.py
+++ exercises/2015-dunman-prelim/task-4/everything.py
@@ -1,12 +1,3 @@
-class TreeNode:
-
-    def __init__(self, pid):
-        self.Lptr = None
-        self.productid = pid
-        self.Rptr = None
-        self.orders = LinkedList()
-
-
 class ListNode:
 
     def __init__(self, oid):
@@ -43,6 +34,15 @@ class LinkedList:
             output.append(current.orderid)
             current = current.next
         return ', '.join(output)
+
+
+class TreeNode:
+
+    def __init__(self, pid):
+        self.Lptr = None
+        self.productid = pid
+        self.Rptr = None
+        self.orders = LinkedList()
 
 
 class BSTree:
@@ -99,13 +99,47 @@ class BSTree:
     def inorder(self):
         self.inorder_func(self.Root)
 
+    def get_all_products(self, root, products):
+        """Return list of tuples with productid and number of orders for each
+        productid.
+        i.e. [(productid, count), ...]
+        """
+        if root is not None:
+            self.get_all_products(root.Lptr, products)
+            products.append((root.productid, root.orders.size()))
+            self.get_all_products(root.Rptr, products)
+
+        return products
+
     def most_popular(self):
-        pass
+        products = self.get_all_products(self.Root, list())
+
+        # sort products in descending order of order count
+        for i in range(len(products)-1):
+            for j in range(len(products)-1-i):
+                if products[j][1] < products[j+1][1]:
+                    temp = products[j]
+                    products[j] = products[j+1]
+                    products[j+1] = temp
+
+        # get product(s) with highest orders
+        highest_orders = products[0][1]
+        best_selling_prods = list()
+        for product in products:
+            no_of_orders = product[1]
+            if no_of_orders == highest_orders:
+                productid = product[0]
+                best_selling_prods.append(productid)
+            else:
+                break
+
+        print("Best-sellers:", ', '.join(best_selling_prods))
 
     def delete(self):
         pass
 
 
+# # Task 4.1
 # tree = BSTree()
 # tree.insert("B", "9")
 # tree.insert("A", "9")
@@ -125,3 +159,6 @@ class BSTree:
 # tree.insert("C", "3")
 # tree.insert("D", "3")
 # tree.inorder()
+
+# # Task 4.2
+# tree.most_popular()
